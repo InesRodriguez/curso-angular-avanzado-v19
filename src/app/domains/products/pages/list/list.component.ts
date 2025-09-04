073cac1b41ce91bd2e2bd959c2a2b371359c37af
Ines Rodriguez <iminesrodriguez@gmail.com>
2025-09-04 17:55:37 -0300

    Add: Replace signal categories by rxResource to managment requested data

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, OnInit, OnChanges, input } from '@angular/core';
+import { Component, inject, signal, OnChanges, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLinkWithHref } from '@angular/router';
 import { ProductComponent } from '@products/components/product/product.component';
@@ -7,26 +7,28 @@ import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 import { CategoryService } from '@shared/services/category.service';
-import { Category } from '@shared/models/category.model';
+import {rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-list',
     imports: [CommonModule, ProductComponent, RouterLinkWithHref],
     templateUrl: './list.component.html'
 })
-export default class ListComponent implements OnInit, OnChanges {
-
-  products = signal<Product[]>([]);
-  categories = signal<Category[]>([]);
+export default class ListComponent implements OnChanges {
   private cartService = inject(CartService);
   private productService = inject(ProductService);
   private categoryService = inject(CategoryService);
+  products = signal<Product[]>([]);
+  
+  categoriesResource = rxResource({
+    loader: () => this.categoryService.getAll(),
+  })
   readonly category_id = input<string>();
   readonly slug = input<string>();
-
-  ngOnInit() {
+// Ya no se usa ngOnInit porque se usa rxResource para obtener los datos ylos estados se manejan con el rxResource
+  /*ngOnInit() {
     this.getCategories();
-  }
+  }*/
 
   ngOnChanges() {
     this.getProducts();
@@ -44,13 +46,19 @@ export default class ListComponent implements OnInit, OnChanges {
       },
     })
   }
+  resetCategories(){
+    this.categoriesResource.set([]);
+  }
+  reloadCategories(){
+    this.categoriesResource.reload();
+  }
 
-  private getCategories() {
+  /*private getCategories() {
     this.categoryService.getAll()
     .subscribe({
       next: (data) => {
         this.categories.set(data);
       },
     })
-  }
+  }*/
 }
