9103eb530a49422a15b0c81e947fb481bc12cbb5
Ines Rodriguez <iminesrodriguez@gmail.com>
2025-09-10 16:11:42 -0300

    Update: ssr config and definition about as prerending page

---
+++ src/app/app.routes.server.ts
@@ -0,0 +1,20 @@
+import { RenderMode, ServerRoute } from '@angular/ssr';
+
+export const serverRoutes: ServerRoute[] = [
+  // About page - can be server-rendered for SEO
+  { path: '', renderMode: RenderMode.Client },
+  { path: 'about', renderMode: RenderMode.Prerender },
+
+  {
+    path: 'category/:slug',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'product/:slug',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'locations',
+    renderMode: RenderMode.Client,
+  },
+];
